e18be11f87460df1af2eb753358401a9c1addc7f
klaus.lippert <lippert@zbmed.de>
2021-06-18 08:43:16 +0200

    don't know, something in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="keywordextraction", 
+    name="keyword_extraction", 
     version="0.1",
     author="Klaus Lippert",
     author_email="",
